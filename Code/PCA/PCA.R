d1bb8bccf94c1167ac872f38ff21925ec8d0e300
Maria Fernandez <maria.fernandez@barcelonagse.eu>
2015-03-14 11:25:54 +0100

    PCA
    updated code, cleaned and included the graphs generated from this code.

--- Code/PCA/PCA.R
+++ Code/PCA/PCA.R
@@ -7,10 +7,14 @@
 
 
 ## USEFULL LINKS:
-## 
+## http://www.jstatsoft.org/v11/i09/paper
+## http://stackoverflow.com/questions/14925942/eigenvalues-vs-pve-percent-variance-explained
+## http://cran.r-project.org/web/packages/kernlab/kernlab.pdf
 
 ## GENERAL INFO:
-# 
+# The PCA looks for the correlation between different variables of our data, and aims to obtain
+# a subset of support vector that includes the maximum of variance of the original data.
+# We are going to apply 3 transformation type: linear, polynomial and radial transformation.
 
 ##-------------------------------------------------------------------------------------------------
 ##Marias directory
@@ -28,7 +32,7 @@ if (!require("kernlab")) install.packages("kernlab")
 if (!require("e1071")) install.packages("e1071")
 if (!require("doMC")) install.packages("doMC")
 if (!require("doParallel")) install.packages("doParallel")
-library(scatterplot3d) 
+if (!require("doParallel")) install.packages("doParallel")
 
 ##-------------------------------------------------------------------------------------------------
 ##INCLUDE DATA
@@ -43,77 +47,6 @@ id_testing <- testing_set$id  ## keep the id
 training_set <- training_set[,-1]  ## remove the id column
 testing_set  <- testing_set[,-1]
 
-##-------------------------------------------------------------------------------------------------
-## New Variables:
-##-------------------------------------------------------------------------------------------------
-
-Cover_Type<-training_set$Cover_Type ## keep the type for later
-training_set<-training_set[,-ncol(training_set)]
-
-
-training_set$vertDistHyd_ElevShift <- (training_set$elevation -training_set$ver_dist_hyd)
-
-#shift elevation by horizontal distance
-
-# 0.6 was selected by choosing the value that reduced the skew of 
-
-# classes 1 and 2 the most (to maximise this overlap group's
-
-# separability
-
-training_set$horDistHyd_ElevShift <- (training_set$elevation -training_set$hor_dist_hyd*0.6)
-
-
-
-#add binary variable for horizontal hydro distance = 0
-
-training_set$zeroHydDist<-0
-
-training_set$zeroHydDist[training_set$hor_dist_hyd==0]<-1
-
-
-
-#add binary variable for those sites with shade (darkness) at 3pm
-
-training_set$shade3pm<-0
-
-training_set$shade3pm[training_set$hill_3pm==0]<-1
-
-
-
-#combine vertical and horizontal values
-
-training_set$crowFliesHyd <- (training_set$hor_dist_hyd^2+training_set$ver_dist_hyd^2)^0.5
-
-
-
-#specific variable that quantifies ability/speed to put out fire if one starts
-
-#to be aligned with pine trees that have higher chance of fire
-
-training_set$fire_risk <- training_set$crowFliesHyd * training_set$hor_dist_fire
-
-
-
-###now same for test
-
-testing_set$vertDistHyd_ElevShift <- (testing_set$elevation -testing_set$ver_dist_hyd)
-
-testing_set$horDistHyd_ElevShift <- (testing_set$elevation -testing_set$hor_dist_hyd*0.6)
-
-testing_set$zeroHydDist<-0
-
-testing_set$zeroHydDist[testing_set$hor_dist_hyd==0]<-1
-
-testing_set$shade3pm<-0
-
-testing_set$shade3pm[testing_set$hill_3pm==0]<-1
-
-testing_set$crowFliesHyd <- (testing_set$hor_dist_hyd^2+testing_set$ver_dist_hyd^2)^0.5
-
-testing_set$fire_risk <- testing_set$crowFliesHyd * testing_set$hor_dist_fire
-
-training_set$Cover_Type<-Cover_Type
 
 ##-------------------------------------------------------------------------------------------------
 ## Preprosesing:
@@ -149,7 +82,7 @@ idx <- idx[sample(1:n)]
 ##shuffel the data
 data<-training_set[idx[1:1500],]
 
-##training linear kernel function
+##training linear kernel function.........................................................
 
 pca.linear<-prcomp(~.,data=data[,1:53])
 pca.linear.variance<-pca.linear$sdev^2/sum(pca.linear$sdev^2)
@@ -159,16 +92,16 @@ for(i in 2:length(pca.linear.variance)){
 }
 
 
-##training polynomial kernel function
+##training polynomial kernel function....................................................
 
-pca.poly<-kpca(~.,data=data[,1:53],kernel="polydot",kpar=list(degree=4)) 
+pca.poly<-kpca(~.,data=data[,1:53],kernel="polydot",kpar=list(degree=2)) 
 pca.poly.variance<-as.numeric(eig(pca.poly)/sum(eig(pca.poly)))
 pca.poly.cum.variance<-pca.poly.variance[1]
 for(i in 2:length(pca.poly.variance)){
   pca.poly.cum.variance[i]<-pca.poly.variance[i]+pca.poly.cum.variance[i-1]
 }
 
-##training  radial kernel function
+##training  radial kernel function.......................................................
 
 pca.radial<-kpca(~.,data=data[,1:53],kernel="rbfdot",kpar=list(sigma=0.1)) 
 pca.radial.variance<-as.numeric(eig(pca.radial)/sum(eig(pca.radial)))
@@ -177,8 +110,13 @@ for(i in 2:length(pca.radial.variance)){
   pca.radial.cum.variance[i]<-pca.radial.variance[i]+pca.radial.cum.variance[i-1]
 }
 
+##-------------------------------------------------------------------------------------------------
+## Results
+##-------------------------------------------------------------------------------------------------
 
-##plot the variance explained by the tree methods
+
+## 1. plot the variance explained by the tree methods
+##-------------------------------------------------------------------------------------------------
 
 exp.variance<-c(pca.linear.cum.variance[1:10],pca.poly.cum.variance[1:10],pca.radial.cum.variance[1:10])
 exp.variance<-cbind(rep(seq(1,10,1),3),exp.variance)
@@ -190,19 +128,51 @@ exp.variance$PC<-as.integer(exp.variance$PC)
 ggplot(exp.variance,aes(x=PC,y=Variance,colour=Method))+geom_line()+theme_bw()+
   scale_x_continuous(breaks=seq(1,10,1))+ylab("Explained Variance")
 
+
+##Export Results:     ----- . ----- . ----- . ----- . ----- . ----- . ----- .
 write.table(exp.variance,"Data/tables/PCA_Variance.csv",sep=";")
+png(filename="Report/Graphs/PCA_Variance", width = 1100, height = 700)
+ggplot(exp.variance,aes(x=PC,y=Variance,colour=Method))+geom_line()+theme_bw()+
+  scale_x_continuous(breaks=seq(1,10,1))+ylab("Explained Variance")
+dev.off()
+## End of Exporting   ----- . ----- . ----- . ----- . ----- . ----- . ----- .
 
 
-vectors<-pcv(pca)[,1:6]
-vectors<-as.data.frame(vectors)
-vectors$type<-train$Cover_Type
+
+## 2. Plot the error using SVM in all the data and with PCA of polynomial
+##-------------------------------------------------------------------------------------------------
 
 
-ksvm(type~.,data=vectors,C=2, kernel="rbfdot",scale=FALSE)
+error<-c()
+for(i in 1:30){ ## loop to see the evolution of the training error, as we include more PC
+  vectors<-pcv(pca.poly)[,1:i]
+  vectors<-as.data.frame(vectors)
+  vectors$type<-data$Cover_Type
+  svm.poly<-ksvm(type~.,data=vectors,C=2, kernel="rbfdot",scale=FALSE)
+  error[i]<-error(svm.poly)
+}
 
-ksvm(Cover_Type~.,data=train,C=2,scale=FALSE)
+svm<-ksvm(Cover_Type~.,data=data,kernel="rbfdot",C=2,scale=FALSE) ## error of SVM with 53 variables
+error.svm<-error(svm)
 
-with(vectors, scatterplot3d(V1, V2, V3, color = as.numeric(type), pch = 19))
-with(train,scatterplot3d(elevation, hor_dist_hyd, ver_dist_hyd, color = as.numeric(Cover_Type), pch = 19))
+error<-cbind(seq(1,30,1),error)
+error.svm<-(cbind(seq(1,30,1),rep(error.svm,30)))
+error<-rbind(error,error.svm)
+error<-as.data.frame(error)
+colnames(error)<-c("PC","error")
+method<-c(rep("PCA",30),rep("NON-PCA",30))
+error$type<-as.factor(method)
+
+
+ggplot(error,aes(x=PC,y=error,colour=type))+geom_line()+theme_bw()+
+  scale_x_continuous(breaks=seq(0,30,5))
+
+##Export Results:     ----- . ----- . ----- . ----- . ----- . ----- . ----- .
+write.table(error,"Data/tables/PCA_error.csv",sep=";")
+png(filename="Report/Graphs/PCA_error", width = 1100, height = 700)
+ggplot(error,aes(x=PC,y=error,colour=type))+geom_line()+theme_bw()+
+  scale_x_continuous(breaks=seq(0,30,5))
+dev.off()
+## End of Exporting   ----- . ----- . ----- . ----- . ----- . ----- . ----- .
 
 
