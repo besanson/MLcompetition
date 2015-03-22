5ec38d2e54974815dc49e40b01bcb2ba9bee7047
Maria Fernandez <maria.fernandez@barcelonagse.eu>
2015-03-22 19:47:10 +0100

    Cleaned Random Forest Code
    Gaston, we still need to include the explanation of the parameters, are
    they number of trees and length of the trees?

--- Code/Method - Random Forest/Method_Random Forest_Code.R
+++ Code/Method - Random Forest/Method_Random Forest_Code.R
@@ -7,18 +7,19 @@
 
 
 ## USEFULL LINKS:
-## MARIA PLEASE
+## http://caret.r-forge.r-project.org
 ## Link to Packages in CRAN
 
 ## GENERAL INFO:
-# MARIA PLEASE
+# We have tried different parameters to see how the training error changed. The following code
+# only includes the selected parameters.
 
+##GASTON WHAT ARE THE PARAMETERS?? number of trees? and lenght of each tree?
 
 
 ##-------------------------------------------------------------------------------------------------
-rm(list=ls())
-
 ## LIBRARIES
+##-------------------------------------------------------------------------------------------------
 
 # Import packages and functions
 ##Packages that are used in this code
@@ -30,87 +31,19 @@ if (!require("doParallel")) install.packages("doParallel")
 
 ##-------------------------------------------------------------------------------------------------
 ##INCLUDE DATA
-
-training_set <- read.table("Kaggle_Covertype_training.csv", sep = ",", header = T)
-testing_set <- read.table("Kaggle_Covertype_test.csv", sep = ",", header = T)
-id_testing <- testing_set$id  ## keep the id 
-training_set <- training_set[,-1]  ## remove the id column
-testing_set  <- testing_set[,-1]
-
 ##-------------------------------------------------------------------------------------------------
-## FEATURE CREATION (Please see Powerpoint presentation for further details)
+rm(list=ls())
 
-##Changes to training set
+cl <- makeCluster(detectCores()) ## detect the cores in the machine
 
-#Shift elevation by vertical distance
-training_set$vertDistHyd_ElevShift <- (training_set$elevation -training_set$ver_dist_hyd)
-
-#Shift elevation by horizontal distance
-# 0.6 was selected by choosing the value that reduced the skew of 
-# classes 1 and 2 the most (to maximise this overlap group's
-# separability
-training_set$horDistHyd_ElevShift <- (training_set$elevation -training_set$hor_dist_hyd*0.6)
-
-#Add binary variable for horizontal hydro distance = 0
-training_set$zeroHydDist<-0
-training_set$zeroHydDist[training_set$hor_dist_hyd==0]<-1
-
-#Add binary variable for those sites with shade (darkness) at 3pm
-training_set$shade3pm<-0
-training_set$shade3pm[training_set$hill_3pm==0]<-1
-
-#Combine vertical and horizontal values
-training_set$crowFliesHyd <- (training_set$hor_dist_hyd^2+training_set$ver_dist_hyd^2)^0.5
-
-#Cpecific variable that quantifies ability/speed to put out fire if one starts
-#to be aligned with pine trees that have higher chance of fire
-training_set$fire_risk <- training_set$crowFliesHyd * training_set$hor_dist_fire
-
-
-#Putting the Cover column last again
-training_set<-cbind(training_set[, -55],Cover_Type=training_set[, 55])
-
-##Changes to Test set
-
-testing_set$vertDistHyd_ElevShift <- (testing_set$elevation -testing_set$ver_dist_hyd)
-testing_set$horDistHyd_ElevShift <- (testing_set$elevation -testing_set$hor_dist_hyd*0.6)
-testing_set$zeroHydDist<-0
-testing_set$zeroHydDist[testing_set$hor_dist_hyd==0]<-1
-testing_set$shade3pm<-0
-testing_set$shade3pm[testing_set$hill_3pm==0]<-1
-testing_set$crowFliesHyd <- (testing_set$hor_dist_hyd^2+testing_set$ver_dist_hyd^2)^0.5
-testing_set$fire_risk <- testing_set$crowFliesHyd * testing_set$hor_dist_fire
-
-##-------------------------------------------------------------------------------------------------
-## PRE-PROCESSING DATA on the Training Set and the Test Set
-
-# Looking if all the variables have variability
-conVarianza <- apply(training_set, 2, function(x) sd(x) != 0) #keep only those that have variability
-conVarianza <- names(conVarianza[conVarianza==T])
-
-training_set <- training_set[, conVarianza]
-
-testing_set  <- testing_set[, conVarianza[1:(length(conVarianza)-1)]]
-
-# Doing some preprocess, as normalizing in this case
-preprocesamiento <- preProcess(rbind(training_set[,-ncol(training_set)], testing_set),
-                               method = c("center", "scale"))
-
+source("Code/ReadData.R")
 
 ##-------------------------------------------------------------------------------------------------
 ##   End of "preparing" the data, now the training begins
 ##-------------------------------------------------------------------------------------------------
 
-#Step on the training set with the normalized data -where the Preprocess is applied through the function predict()-
-#and adding the Cover_Type as a factor
-training_set <- data.frame(predict(preprocesamiento, 
-                                   training_set[,-ncol(training_set)]),
-                           Cover_Type = as.factor(training_set$Cover_Type))
-
 ##-------------------------------------------------------------------------------------------------
 # PREDICTIONS
-
-
 set.seed(4321) ## we need to prove that this is "universal" so we set a new seed
 
 
@@ -125,16 +58,20 @@ fitControl <- trainControl(method = "cv",
 cl <- makeCluster(detectCores()-2)
 registerDoParallel(cl)
 
-# Random Forest with final model
+# Random Forest with final model (the chosen parameters of different tryouts)
 
 rfFit <- train(Cover_Type ~ ., data = training_set,
                method = "rf",
                trControl = fitControl,
                tuneGrid =  data.frame(mtry = c(48)),ntree = c(1000))
 
-predicciones <- predict(rfFit, predict(preprocesamiento, testing_set))
+ypredict <- predict(rfFit, predict(preprocesamiento, testing_set))
 
 stopCluster(cl)
+##-------------------------------------------------------------------------------------------------
+##   Output the results (different files might be created depending on the parameters selection)
+##-------------------------------------------------------------------------------------------------
 
-salida <- data.frame(id =  id_testing, Cover_Type = predicciones)
-write.table(salida, "Pred_Alessio_RF_Final_mtry48_trees1000_CV10.csv", sep = ",", row.names = FALSE, quote = FALSE)
+output <- data.frame(id =  id_testing, Cover_Type = ypredict)
+write.table(output, "Pred_Alessio_RF_Final_mtry48_trees1000_CV10.csv", sep = ",", row.names = FALSE, quote = FALSE)
+
