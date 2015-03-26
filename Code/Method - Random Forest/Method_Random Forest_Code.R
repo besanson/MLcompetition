7c434dd1e669c96788f412352a8ab9dbca27b6cf
JayMack <jordan.j.mciver@gmail.com>
2015-03-26 18:26:36 +0100

    Final Code Review

--- Code/Method - Random Forest/Method_Random Forest_Code.R
+++ Code/Method - Random Forest/Method_Random Forest_Code.R
@@ -8,7 +8,6 @@
 
 ## USEFULL LINKS:
 ## http://caret.r-forge.r-project.org
-## 
 
 ## GENERAL INFO:
 # We have tried different parameters to see how the training error changed. The following code
@@ -27,8 +26,9 @@ loadPackages(c("caret", "e1071","doParallel"))
 ##-------------------------------------------------------------------------------------------------
 ##INCLUDE DATA
 ##-------------------------------------------------------------------------------------------------
-rm(list=ls())
 
+## Clear environment
+rm(list=ls())
 
 source("Code/ReadData.R")
 cl <- makeCluster(detectCores()-1) ## detect the cores in the machine
@@ -40,29 +40,32 @@ cl <- makeCluster(detectCores()-1) ## detect the cores in the machine
 set.seed(4321) ## we need to prove that this is "universal" so we set a new seed
 
 
-#Fit Control Control the computational nuances of the train function
-# In this case, a Cross Validation
+# Fit Control controls the computational nuances of the train function
+# ...in this case, through Cross Validation
 fitControl <- trainControl(method = "cv",
                            number = 10,
                            verboseIter = TRUE)
 
-#Parallel
-#run model in parallel
+# Run model in parallel
 registerDoParallel(cl)
 
-# Random Forest with final model (the chosen parameters of different tryouts)
-# other parameters that have being tried 
-            #ntree= 1000
-            #mtry= 8,16,24,32,40,44,56,59,48
 
+
+## Previous parameters that have being tried 
+# ntree= 1000
+# mtry= 8,16,24,32,40,44,56,59,48
+
+## Random Forest with final model (optimal parameters from previous analyses)
 rfFit <- train(Cover_Type ~ ., data = training_set,
                method = "rf",
                trControl = fitControl,
                tuneGrid =  data.frame(mtry = c(48)),ntree = c(1000))
 
+# Generate prediction
 ypredict <- predict(rfFit, testing_set)
 
 stopCluster(cl)
+
 ##-------------------------------------------------------------------------------------------------
 ##   Output the results (different files might be created depending on the parameters selection)
 ##-------------------------------------------------------------------------------------------------
