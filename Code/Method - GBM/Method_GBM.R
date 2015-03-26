6d1494c7bcb9c6a90b85736f7fde3f5665433c05
JayMack <jordan.j.mciver@gmail.com>
2015-03-26 18:48:12 +0100

    Final Code Review

--- Code/Method - GBM/Method_GBM.R
+++ Code/Method - GBM/Method_GBM.R
@@ -3,15 +3,15 @@
 ##-------------------------------------------------------------------------------------------------
 
 ##DESCRIPTION:
-# this code is going to apply Gradient Boosting Method to the Cover Type Data
+# this code is going to apply the Gradient Boosting Method to the Cover Type Data
 
 
-## USEFULL LINKS:
+## USEFUL LINKS:
 ## http://cran.r-project.org/web/packages/h2o/h2o.pdf
 
 
 ## GENERAL INFO:
-# In used in a laptop run the commented h2o.init in line 
+# If this code is run on a laptop use the commented h2o.init in line, 
 # h2o library already runs it in parallel, so no extra coding is needed
 
 ##-------------------------------------------------------------------------------------------------
@@ -37,26 +37,30 @@ source("Code/ReadData.R") ## all variables
 
 set.seed(4321) 
 
-#Fit Control Control the computational nuances of the train function
+# Fit Control Control the computational nuances of the train function
 
 registerDoParallel(cl)
-
 localH2O <- h2o.init()
 
-##In case that is run in a Laptop use
-#localH2O <- h2o.init(ip = "localhost",
+###################################################
+## If this code is to run on a Laptop, use:
+##
+#  localH2O <- h2o.init(ip = "localhost",
 #                     port = 54321,
 #                     startH2O = TRUE,
 #                     max_mem_size = '2g',
 #                     nthreads = -1)
+###################################################
 
+# Link dataset to h20 cluster
 training_set_h2o <- as.h2o(localH2O, training_set, key = 'training_set')
 
+## Previous parameter values used
+# depth=8,12,16,18,20 
+# trees =100, 250, 500, 1000
+# minobsinnode= 1,5,10
 
-## We trained with depth=8,12,16,18,20 
-                  # trees =100, 250, 500, 1000
-                  # minobsinnode= 1,5,10
-
+# Train GBM model
 trainedGbm <- h2o.gbm(x = 1:(ncol(training_set)-1),
                       y = ncol(training_set),
                       nfolds = 10, # cross validation
@@ -66,11 +70,13 @@ trainedGbm <- h2o.gbm(x = 1:(ncol(training_set)-1),
                       n.minobsinnode = c(10),
                       shrinkage = c(0.1))
 
-# applying the selected method to the testing data set
+# Apply selected method to test data
 testing_set_h2o <- as.h2o(localH2O, predict(preprocesamiento, testing_set), key = 'testing_set')
-# predict the labels
+
+# Make prediction
 ypredict <- h2o.predict(trainedGbm, testing_set_h2o)
 
+# Stop cluster
 stopCluster(cl)
 
 ##-------------------------------------------------------------------------------------------------
